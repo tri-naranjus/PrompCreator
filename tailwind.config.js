c82b53cbf9197633c4f5787b89e6e24dcdf12f03
tri-naranjus <36755608+tri-naranjus@users.noreply.github.com>
2025-04-14 17:33:15 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+// tailwind.config.js
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 200ms ease-in-out',
+      },
+    },
   },
   plugins: [],
 };
