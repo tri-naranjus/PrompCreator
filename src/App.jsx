404e0351349e1e66fe095b2d4b8f5115e89f3ef0
tri-naranjus <36755608+tri-naranjus@users.noreply.github.com>
2025-04-14 09:18:07 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ export default function PromptBuilder() {
         },
         body: JSON.stringify({
           contents: [{
-            parts: [{ text: `Actua como PROFESIONAL DE INGENIERIA DE PROMPOS. Mejora este prompt para que sea más claro y efectivo pero sin que sea muy largo, mejor conciso que exaustivo:
+            parts: [{ text: `Actua como PROFESIONAL DE INGENIERIA DE PROMPOS. Mejora este prompt para que sea más claro y efectivo. Explica que puede empezar con el promp que proponemos y luego interactuar:
 
 ${prompt}` }]
           }]
